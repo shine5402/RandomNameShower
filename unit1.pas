408b532a5d7e9fa7917be8853d646020ea2df46a
Bo <shine_5402@live.cn>
2017-04-01 22:08:10 +0800

    这回主要是让程序记住了关闭时的窗口状态，顺便把里面的一些高级变量也加到ini文件里了。

--- unit1.pas
+++ unit1.pas
@@ -31,6 +31,7 @@ type
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
+    procedure FormShow(Sender: TObject);
     procedure MenuItem3Click(Sender: TObject);
     procedure MenuItem4Click(Sender: TObject);
     procedure MenuItem5Click(Sender: TObject);
@@ -50,8 +51,8 @@ var
   Form1: TForm1;
   names:Tstringlist;
   namespath,settingspath:string;
-  rollnumber,k,i,j:integer;
-  animate,savefontsetting,saveanimatesetting:boolean;
+  rollnumber,k,i,j,animateduration,animatetimes,animateintervalmin,rollnumbermin:integer;
+  animate,savefontsetting,saveanimatesetting,savewindowsize:boolean;
   settings:tinifile;
 implementation
 uses unit2;
@@ -66,20 +67,26 @@ begin
   settings:=Tinifile.create(settingspath);
   namespath:=settings.readstring('paths','namespath','./names.txt');
   names.LoadFromFile(namespath);
-
-
   savefontsetting:=settings.ReadBool('fonts','whethersave',true);
   saveanimatesetting:=settings.ReadBool('animate','whethersave',true);
+  savewindowsize:=settings.ReadBool('window','whethersave',false);
+  if saveanimatesetting then begin
   animate:=settings.ReadBool('animate','switch',false);
+  animateduration:=settings.ReadInteger('advanced','animte.animteduration',2000);
+  rollnumbermin:=settings.ReadInteger('advanced','animate.rollnumbermin',20);
+  animateintervalmin:=settings.ReadInteger('advanced','animate.animateintervalmin',1);
+  end;
   if savefontsetting then begin
   fontdialog1.Font.Size:=settings.ReadInteger('font','size',18);
   fontdialog1.Font.color:=settings.readinteger('font','color',clDefault);
   fontdialog1.Font.name:=settings.Readstring('font','fontname','微软雅黑');
   label1.Font:=fontdialog1.Font;
   end;
+
   MenuItem5.Checked:=animate;
   rollnumber:=names.count+1;
-  if 2000 div rollnumber <> 0 then Timer1.Interval:=3000 div rollnumber else Timer1.Interval:=1;
+  if rollnumber < rollnumbermin then rollnumber:=rollnumbermin;
+  if animateduration div rollnumber <> 0 then Timer1.Interval:=animateduration div rollnumber else Timer1.Interval:=animateintervalmin;
     i:=names.count;
   k:=0;
   randomize;
@@ -91,6 +98,7 @@ begin
   settings.WriteString('paths','namespath',namespath);
   settings.WriteBool('fonts','whethersave',savefontsetting);
   settings.WriteBool('animate','whethersave',saveanimatesetting);
+  settings.WriteBool('window','whethersave',savewindowsize);
   if savefontsetting then begin
   settings.writeinteger('font','size',fontdialog1.Font.Size);
   settings.writeinteger('font','color',fontdialog1.Font.color);
@@ -98,8 +106,21 @@ begin
   end;
   if saveanimatesetting then begin
   settings.WriteBool('animate','switch',animate);
+  settings.WriteInteger('advanced','animte.animteduration',animateduration);
+  settings.WriteInteger('advanced','animate.rollnumbermin',rollnumbermin);
+  settings.WriteInteger('advanced','animate.animateintervalmin',animateintervalmin);
   end;
+
   settings.Free;
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+  if savewindowsize then begin
+  Form1.SetBounds(settings.ReadInteger('window','left',169),settings.ReadInteger('window','top',491),settings.ReadInteger('window','width',339),settings.ReadInteger('window','height',274));
+
+  Form1.WindowState:=TWindowState(settings.ReadInteger('window','WindowState',Integer(wsNormal)));
+  end;
 end;
 
 procedure TForm1.MenuItem3Click(Sender: TObject);
@@ -162,14 +183,18 @@ begin
   if animate then timer1.Enabled:=true else begin
   j:=random(i);
   Label1.Caption:=names.Strings[j];
-
-
   end;
 end;
 
 procedure TForm1.FormClose(Sender: TObject; var CloseAction: TCloseAction);
 begin
-
+  if savewindowsize then begin
+  settings.WriteInteger('window','top',Form1.top);
+  settings.WriteInteger('window','left',Form1.left);
+  settings.WriteInteger('window','width',Form1.Width);
+  settings.WriteInteger('window','height',Form1.Height);
+  settings.WriteInteger('window','WindowState',Integer(Form1.WindowState));
+  end;
 end;
 
 
