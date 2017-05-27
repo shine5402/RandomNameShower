44815a13bd189549f22d5fad9b1c46d7c8c4c793
Bo <shine_5402@live.cn>
2017-05-27 20:21:17 +0800

    增加启动窗口

--- project1.lpr
+++ project1.lpr
@@ -7,7 +7,7 @@ uses
   cthreads,
   {$ENDIF}{$ENDIF}
   Interfaces, // this includes the LCL widgetset
-  Forms, Unit1, Unit3, Unit2, Unit4
+  Forms, Unit1, Unit3, Unit2, Unit4, Unit5
   { you can add units after this };
 
 {$R *.res}
@@ -16,10 +16,11 @@ begin
   Application.Title:='随机点名器';
   RequireDerivedFormResource:=True;
   Application.Initialize;
+  Application.CreateForm(TLoadingForm, LoadingForm);
   Application.CreateForm(TForm1, Form1);
-  Application.CreateForm(TAboutForm, AboutForm);
   Application.CreateForm(TForm2, Form2);
   Application.CreateForm(TForm3, Form3);
+  Application.CreateForm(TAboutForm, AboutForm);
   Application.Run;
 end.
 
---
+++ unit5.pas
@@ -0,0 +1,143 @@
+unit Unit5;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ComCtrls,
+  StdCtrls,unit1,unit2,unit3,unit4,inifiles,md5,windows;
+const
+  UM_CHECKFIRSTRUN = WM_USER + 100;
+
+type
+
+  { TLoadingForm }
+
+  TLoadingForm = class(TForm)
+    Label1: TLabel;
+    ProgressBar1: TProgressBar;
+    procedure FormActivate(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure ProgressBar1Exit(Sender: TObject);
+  private
+    { private declarations }
+    procedure UmCheckFirstRun(var Message: TMessage); message UM_CHECKFIRSTRUN;
+  public
+    { public declarations }
+  end;
+
+var
+  LoadingForm: TLoadingForm;
+  first:boolean;
+implementation
+
+{$R *.lfm}
+
+{ TLoadingForm }
+
+procedure TLoadingForm.FormCreate(Sender: TObject);
+begin
+  first:=true;
+  PostMessage(Handle, UM_CHECKFIRSTRUN, 0, 0);
+end;
+
+procedure TLoadingForm.FormShow(Sender: TObject);
+begin
+
+end;
+procedure TLoadingform.UmCheckFirstRun(var Message: TMessage);
+begin
+  if first then begin
+loadingform.label1.caption:='加载上一次的设置……';
+progressbar1.position:=20;
+rootenabled:=false;
+names:=Tstringlist.Create;
+settingspath:='./settings.ini';
+settings:=Tinifile.create(settingspath);
+namespath:=settings.readstring('paths','namespath','./names.txt');
+passwordmd5:=settings.readstring('password','md5','25d55ad283aa400af464c76d713c07ad');
+names.LoadFromFile(namespath);
+savefontsetting:=settings.ReadBool('font','whethersave',true);
+saveanimatesetting:=settings.ReadBool('animate','whethersave',true);
+savewindowsize:=settings.ReadBool('window','whethersave',false);
+whetherhash:=settings.readbool('hash','whetherhash',false);
+encrypthash:=settings.readbool('hash','encrypthash',false);
+encryptkey:=settings.readinteger('hash','encryptkey',1234);
+hashencryptlength:=settings.readinteger('hash','hashencryptlength',0);
+loadingform.label1.caption:='应用设置……';
+progressbar1.position:=40;
+if saveanimatesetting then begin
+animate:=settings.ReadBool('animate','switch',false);
+animateduration:=settings.ReadInteger('advanced','animte.animteduration',2000);
+rollnumbermin:=settings.ReadInteger('advanced','animate.rollnumbermin',20);
+animateintervalmin:=settings.ReadInteger('advanced','animate.animateintervalmin',1);
+end;
+if savefontsetting then begin
+form1.fontdialog1.Font.Size:=settings.ReadInteger('font','size',18);
+form1.fontdialog1.Font.color:=settings.readinteger('font','color',clDefault);
+form1.fontdialog1.Font.name:=settings.Readstring('font','fontname','微软雅黑');
+form1.label1.Font:=form1.fontdialog1.Font;
+end;
+loadingform.label1.caption:='校验文件Hash值，如果卡顿超过3分钟，请退出重试……';
+progressbar1.position:=60;
+if encrypthash then begin
+nameshash:=MD5Print(MD5File(namespath));
+nameslasthash:='';
+for i:= 1 to hashencryptlength do begin
+  str(i,s);
+  s:='e'+s;
+  j:=settings.readinteger('hash',s,0) xor encryptkey;
+  //showmessage(chr(j));
+  //showmessage(nameshash);
+  nameslasthash:=nameslasthash+chr(j);
+  //showmessage(nameslasthash);
+end;
+if nameshash <> nameslasthash then begin
+showmessage('名单文件的Hash记录与上一次记录的不符，建议检查名单文件有没有被篡改！程序即将退出。');
+application.Terminate;
+end;
+end;
+if (whetherhash xor encrypthash) and whetherhash then begin
+nameslasthash:=settings.readstring('hash','namesmd5','');
+if nameslasthash = '' then begin
+showmessage('没有找到上一次的名单Hash记录，settings.ini中的记录可能被删除，建议检查名单文件有没有被篡改！程序即将退出。');
+application.Terminate;
+end;
+nameshash:=MD5Print(MD5File(namespath));
+if nameshash <> nameslasthash then begin
+showmessage('名单文件的Hash记录与上一次记录的不符，建议检查名单文件有没有被篡改！程序即将退出。');
+application.Terminate;
+end;
+end;
+if passwordmd5='25d55ad283aa400af464c76d713c07ad' then showmessage('您的管理员密码目前为初始密码。请进入选项进行修改。');
+loadingform.label1.caption:='应用设置……';
+progressbar1.position:=80;
+form1.MenuItem5.Checked:=animate;
+rollnumber:=names.count+1;
+if rollnumber < rollnumbermin then rollnumber:=rollnumbermin;
+if animateduration div rollnumber <> 0 then form1.Timer1.Interval:=animateduration div rollnumber else form1.Timer1.Interval:=animateintervalmin;
+  i:=names.count;
+k:=0;
+j:=0;
+randomize;
+loadingform.label1.caption:='加载完成！';
+progressbar1.position:=100;
+form1.show;
+loadingform.hide;
+loadingform.release;
+end;
+end;
+procedure TLoadingForm.ProgressBar1Exit(Sender: TObject);
+begin
+
+end;
+
+procedure TLoadingForm.FormActivate(Sender: TObject);
+begin
+
+end;
+
+end.
+
